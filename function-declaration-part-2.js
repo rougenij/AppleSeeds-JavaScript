4cec2ad2ba30b6b8e5001adcd55c0bd5737d3a14
Rouge Nij <rougenij@gmail.com>
2021-11-08 13:28:58 +0200

    JS day 2 Ex

--- function-declaration-part-2.js
+++ function-declaration-part-2.js
@@ -10,18 +10,30 @@
 */
 
 // From function declarations to explicit and implicit return functions (one of each).
-function welcome() {
-    let welcome = 'Welcome to Appleseeds Bootcamp!';
-    return welcome;
-}
-
-function power(a) {
-    let myNumber = a;
-    let result = Math.pow(myNumber, 2);
-    return result;
-}
+// function welcome() {
+//     let welcome = 'Welcome to Appleseeds Bootcamp!';
+//     return welcome;
+// }
+const welcome = () => {
+    console.log("Welcome to AppleSeedsBootcamp!");
+    }
+// function power(a) {
+//     let myNumber = a;
+//     let result = Math.pow(myNumber, 2);
+//     return result;
+// }
+const power = a => Math.pow(a)
 
 // From function expressions to IIFE functions.
-const squareRoot = a => Math.sqrt(a);
+// const squareRoot = a => Math.sqrt(a);
+(function(){
+    let a;
+    Math.sqrt(a)
+})();
 
-const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+// const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+(function(){
+    let a;
+    let b;
+    Math.random() * (a - b) + b;
+})();
