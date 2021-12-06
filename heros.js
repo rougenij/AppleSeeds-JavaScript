3d3604f00e2583b6ce36b4a9e17ba0880210ab16
Rouge Nij <rougenij@gmail.com>
2021-12-06 19:28:29 +0200

    Call bind Apply

--- heros.js
+++ heros.js
@@ -9,11 +9,31 @@ const superHeroes = [wonderWoman, batman];
 function printName() {
   console.log(`my name is ${this.name}`);
 }
-
+//! Normal For Loop
 function printHeroes(heroes, printFunc) {
   for (let i = 0; i < heroes.length; i++) {
     printFunc.call(heroes[i]);
   }
 }
+printHeroes(superHeroes, printName);
 
-printHeroes(superHeroes, printName());
+//! For of Loop
+function printHeroes(heroes, printFunc) {
+  for (let hero of heroes) {
+    printFunc.call(hero);
+  }
+}
+
+printHeroes(superHeroes, printName);
+
+//! ForEach loop
+function printHeroes(heroes, printFunc) {
+  heroes.forEach((hero) => {
+    printFunc.call(hero);
+  });
+}
+printHeroes(superHeroes, printName);
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
